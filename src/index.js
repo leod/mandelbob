15445c5265261aa05f2ae5c716fd4dbfc2eb7b53
Leonard Dahlmann <leo.dahlmann@gmail.com>
2018-09-16 18:12:38 +0200

    Use camera in shader

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import * as THREE from 'three';
 import { ScreenQuad } from './screen-quad';
 import { Controls } from './controls';
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+var container = document.getElementById("view");
 
-var screenQuad = new ScreenQuad(window.innerWidth, window.innerHeight, {
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(container.clientWidth, container.clientHeight);
+console.log(container.clientWidth);
+console.log(container.clientHeight);
+
+var screenQuad = new ScreenQuad(container.clientWidth, container.clientHeight, {
     time: { type: "f", value: 1.0 },
+    center: { type: "v2", value: new THREE.Vector2(0.0, 0.0) },
+    zoom: { type: "f", value: 1.0 },
 })
 
-document.body.appendChild(renderer.domElement);
+container.appendChild(renderer.domElement);
 
 var frameStartTime = Date.now();
 
@@ -21,8 +27,13 @@ function animate() {
     var nowTime = Date.now();
     var elapsedSeconds = (nowTime - frameStartTime) / 1000.0;
     frameStartTime = nowTime;
-    
+
     controls.update(elapsedSeconds);
+
+    screenQuad.uniforms.center.value.x = controls.center.x;
+    screenQuad.uniforms.center.value.y = controls.center.y;
+    screenQuad.uniforms.zoom.value = controls.zoom;
+    
     renderer.render(screenQuad.scene, screenQuad.camera);
 }
 
