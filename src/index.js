d7a4ee7906bdeb8e35b7a2a77471da2b00327a4c
leod <subtle.frustration@proton.me>
2018-09-16 13:21:10 +0200

    Screen rect pixel shader

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
 import * as THREE from 'three';
 
-alert('hi');
-
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var camera = new THREE.Camera();
+camera.position.z = 1.0;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var uniforms = {
+    time: { type: "f", value: 1.0 },
+    resolution: { type: "v2", value: new THREE.Vector2() }
+};
 
-camera.position.z = 5;
+var material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+});
+
+var mesh = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), material);
+scene.add(mesh);
+
+uniforms.resolution.value.x = window.innerWidth;
+uniforms.resolution.value.y = window.innerHeight;
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+var startTime = Date.now();
 
 function animate() {
-	requestAnimationFrame(animate);
-	renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    render();
 }
+
+function render() {
+    var elapsedMilliseconds = Date.now() - startTime;
+    var elapsedSeconds = elapsedMilliseconds / 1000.;
+    uniforms.time.value = 60. * elapsedSeconds;
+    renderer.render(scene, camera);
+}
+
 animate();
